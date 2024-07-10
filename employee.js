d08a89ea1f9391a8d006cc3efb10b909f1d623d4
CECAYA <charlesedouardcay@gmail.com>
2024-07-10 12:10:40 -0400

    Update employee.js

--- employee.js
+++ employee.js
@@ -4,10 +4,13 @@ import { db } from './config.js';
 // Fonction pour appeler le prochain usager
 function callNextUser() {
     const counterNumber = document.getElementById('counterNumber').value;
-    if (counterNumber) {
+    const roomNumber = document.getElementById('roomNumber').value; // Récupérer le numéro de salle
+
+    if (counterNumber && roomNumber) { // Vérifiez que les deux champs sont remplis
         setDoc(doc(db, 'waitingRoom', 'current'), {
             number: increment(1),
-            counter: counterNumber
+            counter: counterNumber,
+            room: roomNumber // Ajouter le numéro de salle
         }, { merge: true });
     }
 }
