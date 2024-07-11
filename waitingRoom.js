e9b74ebe7d50e9fafc32ba3f5f283c87c1c14ecb
CECAYA <charlesedouardcay@gmail.com>
2024-07-11 10:16:05 -0400

    Update waitingRoom.js

--- waitingRoom.js
+++ waitingRoom.js
@@ -1,21 +1,31 @@
 import { doc, onSnapshot } from "https://www.gstatic.com/firebasejs/9.0.0/firebase-firestore.js";
 import { db } from './config.js';
 
-// Fonction pour afficher le numéro appelé, le comptoir et la salle
-function formatNumber1(num3) {
-    return num3.toString().padStart(2, '0'); // Utilise padStart pour ajouter un 0 devant si nécessaire
+// Fonction pour formater le numéro
+function formatNumber1(num) {
+    return num.toString().padStart(2, '0'); // Utilise padStart pour ajouter un 0 devant si nécessaire
 }
 
+// Écoute les changements dans le document 'current' de la collection 'waitingRoom'
 onSnapshot(doc(db, 'waitingRoom', 'current'), (doc) => {
-    if (doc.exists) {
-        const number2 = doc.data().number;
-        document.getElementById('currentNumber').textContent = formatNumber1(number2);
-        document.getElementById('counterNumber').textContent = doc.data().counter;
-        document.getElementById('roomNumber').textContent = doc.data().room; // Afficher le numéro de salle
-        document.getElementById('oldNumber1').textContent = doc.data().old1;
-        document.getElementById('oldNumber2').textContent = doc.data().old2;
-        document.getElementById('oldNumber3').textContent = doc.data().old3;
-        document.getElementById('oldNumber4').textContent = doc.data().old4;
-        document.getElementById('oldNumber5').textContent = doc.data().old5;
+    if (doc.exists()) {
+        const data = doc.data();
+        const number = data.number;
+        const counter = data.counter;
+        const room = data.room;
+        const old1 = data.old1;
+        const old2 = data.old2;
+        const old3 = data.old3;
+        const old4 = data.old4;
+        const old5 = data.old5;
+
+        document.getElementById('currentNumber').textContent = formatNumber1(number);
+        document.getElementById('counterNumber').textContent = counter;
+        document.getElementById('roomNumber').textContent = room; // Afficher le numéro de salle
+        document.getElementById('oldNumber1').textContent = old1;
+        document.getElementById('oldNumber2').textContent = old2;
+        document.getElementById('oldNumber3').textContent = old3;
+        document.getElementById('oldNumber4').textContent = old4;
+        document.getElementById('oldNumber5').textContent = old5;
     }
 });
