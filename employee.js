8597ff375cc7651b202e3906177daacda010ecb3
CECAYA <charlesedouardcay@gmail.com>
2024-07-10 23:14:58 -0400

    Update employee.js

--- employee.js
+++ employee.js
@@ -1,31 +1,37 @@
-import { doc, setDoc, increment } from "https://www.gstatic.com/firebasejs/9.0.0/firebase-firestore.js";
+import { doc, setDoc, increment, getDoc, updateDoc } from "https://www.gstatic.com/firebasejs/9.0.0/firebase-firestore.js";
 import { db } from './config.js';
 
 // Fonction pour appeler le prochain usager
-function callNextUser() {
-    const counterNumber = document.getElementById('counterNumber').value;
-    const roomNumber = document.getElementById('roomNumber').value; // Récupérer le numéro de salle
+async function callNextUser() {
+    const docRef = doc(db, 'waitingRoom', 'current');
+    const docSnap = await getDoc(docRef);
 
-    if (counterNumber && roomNumber) { // Vérifiez que les deux champs sont remplis
-        let nextNumber = increment(1); // Incrémenter le numéro actuel
+    if (docSnap.exists()) {
+        const currentNumber = docSnap.data().number;
+        const counterNumber = document.getElementById('counterNumber').value;
+        const roomNumber = document.getElementById('roomNumber').value; // Récupérer le numéro de salle
 
-        // Vérifier si le prochain numéro est 100
-        if (nextNumber >= 100) {
-            nextNumber = 0; // Remettre à 0 si le numéro atteint 100
+        if (counterNumber && roomNumber) { // Vérifiez que les deux champs sont remplis
+            let nextNumber = increment(currentNumber + 1); // Incrémenter le numéro actuel
+
+            // Vérifier si le prochain numéro est 100
+            if (nextNumber >= 100) {
+                nextNumber = 0; // Remettre à 0 si le numéro atteint 100
+            }
+
+            // Mettre à jour les données dans Firebase
+            await setDoc(docRef, {
+                number: nextNumber,
+                counter: counterNumber,
+                room: roomNumber // Ajouter le numéro de salle
+            }, { merge: true });
         }
-
-        // Mettre à jour les données dans Firebase
-        setDoc(doc(db, 'waitingRoom', 'current'), {
-            number: nextNumber,
-            counter: counterNumber,
-            room: roomNumber // Ajouter le numéro de salle
-        }, { merge: true });
     }
 }
 
 // Nouvelle fonction pour réinitialiser le compteur
-function resetCounter() {
-    setDoc(doc(db, 'waitingRoom', 'current'), {
+async function resetCounter() {
+    await updateDoc(doc(db, 'waitingRoom', 'current'), {
         number: 0,
         counter: "?",
         room: "?"
