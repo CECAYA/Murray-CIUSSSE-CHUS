659e2721c1a72b85d28a8f25f74b617fe835f4ce
CECAYA <charlesedouardcay@gmail.com>
2024-07-10 23:07:22 -0400

    Update employee.js

--- employee.js
+++ employee.js
@@ -7,8 +7,16 @@ function callNextUser() {
     const roomNumber = document.getElementById('roomNumber').value; // Récupérer le numéro de salle
 
     if (counterNumber && roomNumber) { // Vérifiez que les deux champs sont remplis
+        let nextNumber = increment(1); // Incrémenter le numéro actuel
+
+        // Vérifier si le prochain numéro est 100
+        if (nextNumber >= 100) {
+            nextNumber = 0; // Remettre à 0 si le numéro atteint 100
+        }
+
+        // Mettre à jour les données dans Firebase
         setDoc(doc(db, 'waitingRoom', 'current'), {
-            number: increment(1),
+            number: nextNumber,
             counter: counterNumber,
             room: roomNumber // Ajouter le numéro de salle
         }, { merge: true });
