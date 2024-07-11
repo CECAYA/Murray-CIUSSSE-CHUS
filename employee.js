bd884733b57669f835588ffb7420ab091255f557
CECAYA <charlesedouardcay@gmail.com>
2024-07-11 09:51:17 -0400

    Update employee.js

--- employee.js
+++ employee.js
@@ -5,6 +5,11 @@ import { db } from './config.js';
 async function callNextUser() {
     const counterNumber = document.getElementById('counterNumber').value;
     const roomNumber = document.getElementById('roomNumber').value;
+    const oldNumber1 = document.getElementById('oldNumber1').value;
+    const oldNumber2 = document.getElementById('oldNumber2').value;
+    const oldNumber3 = document.getElementById('oldNumber3').value;
+    const oldNumber4 = document.getElementById('oldNumber4').value;
+    const oldNumber5 = document.getElementById('oldNumber5').value;
 
     if (counterNumber && roomNumber) {
         const docRef = doc(db, 'waitingRoom', 'current');
@@ -23,6 +28,11 @@ async function callNextUser() {
         }
 
         await setDoc(docRef, {
+            old5: oldNumber4,
+            old4: oldNumber3,
+            old3: oldNumber2,
+            old2: oldNumber1,
+            old1: currentNumber,
             number: newNumber,
             counter: counterNumber,
             room: roomNumber
