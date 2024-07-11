5bae55daeb6b4e18b2057e1851d69527ae56898e
CECAYA <charlesedouardcay@gmail.com>
2024-07-11 00:04:37 -0400

    Update waitingRoom.js

--- waitingRoom.js
+++ waitingRoom.js
@@ -2,9 +2,14 @@ import { doc, onSnapshot } from "https://www.gstatic.com/firebasejs/9.0.0/fireba
 import { db } from './config.js';
 
 // Fonction pour afficher le numéro appelé, le comptoir et la salle
+function formatNumber(num) {
+    return num.toString().padStart(2, '0'); // Utilise padStart pour ajouter un 0 devant si nécessaire
+}
+
 onSnapshot(doc(db, 'waitingRoom', 'current'), (doc) => {
     if (doc.exists) {
-        document.getElementById('currentNumber').textContent = doc.data().number;
+        const number = doc.data().number;
+        document.getElementById('currentNumber').textContent = formatNumber(number);
         document.getElementById('counterNumber').textContent = doc.data().counter;
         document.getElementById('roomNumber').textContent = doc.data().room; // Afficher le numéro de salle
     }
