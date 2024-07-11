cd2a3028c79b4f0750c18675cb6efdb2029cb980
CECAYA <charlesedouardcay@gmail.com>
2024-07-11 10:34:29 -0400

    Update waitingRoom.js

--- waitingRoom.js
+++ waitingRoom.js
@@ -1,6 +1,11 @@
 import { doc, onSnapshot } from "https://www.gstatic.com/firebasejs/9.0.0/firebase-firestore.js";
 import { db } from './config.js';
 
+// Fonction pour formater le numéro avec la salle et le poste
+function formatOldNumbers(oldNumbers) {
+    return oldNumbers.map(num => `${num.number} - ${num.room} - ${num.counter}`).join('<br>');
+}
+
 // Fonction pour afficher le numéro appelé, le comptoir et la salle
 function formatNumber1(num3) {
     return num3.toString().padStart(2, '0'); // Utilise padStart pour ajouter un 0 devant si nécessaire
@@ -13,10 +18,8 @@ onSnapshot(doc(db, 'waitingRoom', 'current'), (doc) => {
         document.getElementById('counterNumber').textContent = data.counter;
         document.getElementById('roomNumber').textContent = data.room; // Afficher le numéro de salle
 
-        // Afficher les anciens numéros
+        // Afficher les anciens numéros avec la salle et le poste
         const oldNumbers = data.oldNumbers || [];
-        for (let i = 0; i < 5; i++) {
-            document.getElementById(`old${i + 1}`).textContent = oldNumbers[i] !== undefined ? formatNumber1(oldNumbers[i]) : '-';
-        }
+        document.getElementById('oldNumbers').innerHTML = formatOldNumbers(oldNumbers);
     }
 });
