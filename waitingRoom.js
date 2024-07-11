9115eca9be6a8ec18cf6b072b0fbaa11b4dcb073
CECAYA <charlesedouardcay@gmail.com>
2024-07-11 15:57:46 -0400

    Update waitingRoom.js

--- waitingRoom.js
+++ waitingRoom.js
@@ -32,10 +32,6 @@ onSnapshot(doc(db, 'waitingRoom', 'current'), (doc) => {
         // Afficher le temps moyen
         document.getElementById('tempsMoyen').textContent = tempsMoyen.toFixed(2);
 
-        // Jouer le son de notification
-        const notification123 = document.getElementById('notification123');
-        if (notification123) {
-            notification123.play();
         }
     }
 });
