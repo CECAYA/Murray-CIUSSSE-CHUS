b0740a861348e3b668b38c90c7e297fb00e11e84
CECAYA <charlesedouardcay@gmail.com>
2024-07-25 16:56:19 -0400

    Update waitingRoom.js

--- waitingRoom.js
+++ waitingRoom.js
@@ -6,17 +6,6 @@ function formatNumber1(num3) {
     return num3.toString().padStart(2, '0'); // Utilise padStart pour ajouter un 0 devant si nécessaire
 }
 
-function bouttonOff(temps) {
-    const nextButton = document.querySelector('.button1');
-    nextButton.disabled = true;
-    nextButton.classList.add('button-disabled');
-
-    // Attendre 3 secondes (3000 millisecondes)
-    setTimeout(() => {
-        nextButton.disabled = false;
-        nextButton.classList.remove('button-disabled');
-    }, temps);
-}
 
 
 function speakNumber(number) {
