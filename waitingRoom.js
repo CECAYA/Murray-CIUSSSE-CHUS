a7e3f3c360610c9e53bb5219699b5fe245ed0dd8
CECAYA <charlesedouardcay@gmail.com>
2024-07-26 11:55:32 -0400

    Update waitingRoom.js

--- waitingRoom.js
+++ waitingRoom.js
@@ -1,17 +1,14 @@
 import { doc, onSnapshot } from "https://www.gstatic.com/firebasejs/9.0.0/firebase-firestore.js";
 import { db } from './config.js';
 
-// Fonction pour afficher le numéro appelé, le comptoir et la salle
 function formatNumber1(num3) {
-    return num3.toString().padStart(2, '0'); // Utilise padStart pour ajouter un 0 devant si nécessaire
+    return num3.toString().padStart(2, '0');
 }
-
-
 
 function speakNumber(number) {
     const speech = new SpeechSynthesisUtterance();
-    speech.lang = 'fr-FR'; // Langue française
-    speech.text = `Numéro : ${number}`; // Utilisation directe de la variable number
+    speech.lang = 'fr-FR';
+    speech.text = `Numéro : ${number}`;
     speech.volume = 1;
     speech.rate = 1;
     speech.pitch = 1;
@@ -24,42 +21,33 @@ onSnapshot(doc(db, 'waitingRoom', 'current'), (doc) => {
         const data = doc.data();
         document.getElementById('currentNumber').textContent = formatNumber1(data.number);
         document.getElementById('counterNumber').textContent = data.counter;
-        document.getElementById('roomNumber').textContent = data.room; // Afficher le numéro de salle
+        document.getElementById('roomNumber').textContent = data.room;
 
-        // Afficher les anciens numéros
         const oldNumbers = data.oldNumbers || [];
         for (let i = 0; i < 5; i++) {
             document.getElementById(`old${i + 1}`).textContent = oldNumbers[i] !== undefined ? oldNumbers[i] : '-';
         }
         const oldTimes = data.oldTimes || [];
-
-        // Calculer la somme des différences entre les oldTimes
         let totalDifference = 0;
         for (let i = 0; i < oldTimes.length - 1; i++) {
             totalDifference += oldTimes[i] - oldTimes[i + 1];
         }
-
-        // Calculer le temps moyen en minutes
         const tempsMoyen = oldTimes.length > 1 ? (totalDifference / (oldTimes.length - 1)) / 60000 : 0;
-
-        // Afficher le temps moyen
         document.getElementById('tempsMoyen').textContent = tempsMoyen.toFixed(2);
 
-        if (data.disponible = false){
-            BouttonOff();
-            if (data.room != "?"){
-            // Jouer le son de notification
-            const notification123 = document.getElementById('notification123');
+        if (data.disponible === false) {
+            bouttonOff();
+            if (data.room != "?") {
+                const notification123 = document.getElementById('notification123');
                 if (notification123) {
-                notification123.play();
-                
+                    notification123.play();
                     setTimeout(() => {
-                    speakNumber(data.number);
+                        speakNumber(data.number);
                     }, 1500);
                 }
             }
         } else {
-            BouttonOn();
+            bouttonOn();
         }
     }
 });
