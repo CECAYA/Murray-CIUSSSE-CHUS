bbc3ec6f674072c851a58fff4fa17a5f54fd4b0a
CECAYA <charlesedouardcay@gmail.com>
2024-07-26 12:33:25 -0400

    Update waitingRoom.js

--- waitingRoom.js
+++ waitingRoom.js
@@ -16,6 +16,19 @@ function speakNumber(number) {
     window.speechSynthesis.speak(speech);
 }
 
+function bouttonOff() {
+    const nextButton = document.querySelector('.button1');
+    nextButton.disabled = true;
+    nextButton.classList.add('button-disabled');
+}
+
+function bouttonOn() {
+    const nextButton = document.querySelector('.button1');
+    nextButton.disabled = false;
+    nextButton.classList.remove('button-disabled');
+}
+
+
 onSnapshot(doc(db, 'waitingRoom', 'current'), (doc) => {
     if (doc.exists) {
         const data = doc.data();
@@ -52,3 +65,6 @@ onSnapshot(doc(db, 'waitingRoom', 'current'), (doc) => {
         }
     }
 });
+
+window.bouttonOff = bouttonOff;
+window.bouttonOn = bouttonOn;
