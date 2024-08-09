846d4ea028281a22174192fa7f95185ea7b9167c
CECAYA <charlesedouardcay@gmail.com>
2024-08-09 14:04:38 -0400

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,7 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.0.0/firebase-app.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/9.0.0/firebase-firestore.js";
+import { getAuth, signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/9.0.0/firebase-auth.js";
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyAElbh1G-0B5xadXbLNxFBxtHXLo8eRcJE",
   authDomain: "murray-ciussse-chus-d010a.firebaseapp.com",
@@ -13,6 +12,6 @@ const firebaseConfig = {
   measurementId: "G-1T54V9VL43"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
+export const auth = getAuth(app);
